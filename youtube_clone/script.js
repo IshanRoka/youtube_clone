cf3e76adf68286e273ab3846b42fdb82e7019826
IshanRoka <116943515+IshanRoka@users.noreply.github.com>
2024-06-28 21:10:53 +0545

    Add files via upload

---
+++ youtube_clone/script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+const menuIcon=document.querySelector(".menu-icon")
+const sideBar=document.querySelector(".sidebar")
+const container=document.querySelector(".container")
+
+menuIcon.addEventListener("click",()=>{
+    sideBar.classList.toggle("small-sidebar")
+    container.classList.toggle("large-container")
+})
+
+
+
+})
